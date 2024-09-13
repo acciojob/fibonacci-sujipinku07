fb9df97b354719301e52fc002d324d18e0052266
sujipinku07 <sujipinku07@gmail.com>
2024-09-13 20:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function fibonacci(num) {
-if(num===1) {
-	return 0;
-}	if(n===2) {
+	if(num === 1) 
+		return 0;
+	else if(num === 2)
 		return 1;
-	} 
-	return fib(n-1)+fib(n-2);// your code here
+	num -= 2;
+	var num1 = 0;
+	var num2 = 1;
+	var sum;
+	var i = 0;
+	for(i=0; i < num; i++) {
+		sum = num1+ num2;
+		num1 = num2;
+		num2 = sum;
+	}
+	return num2;
 }
-
-module.exports = fibonacci;
+module.exports= fibonacci;
